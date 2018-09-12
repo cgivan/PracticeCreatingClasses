0ac1e2759e97d576f4f61e3dfaf5f44f1174aa67
Chris Givan <chrisgivan502@gmail.com>
2018-09-12 10:45:59 -0400

    Add project files.

---
+++ PracticeCreatingClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeCreatingClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
